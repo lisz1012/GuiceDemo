f021d4da308bb8354d8fdd3162fa738440e84c91
lisz1012 <lisz1012@163.com>
2020-07-17 01:40:36 -0700

    install method used, about to add the tests

--- src/main/java/com/lisz/AddressModule.java
+++ src/main/java/com/lisz/AddressModule.java
@@ -1,12 +1,11 @@
 package com.lisz;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.name.Names;
 
 public class AddressModule extends AbstractModule {
 	@Override
 	protected void configure() {
-		//super.configure();
-		bind(int.class).toInstance(100);
-		bind(String.class).toInstance("st");
+		bind(Address.class).annotatedWith(Names.named("address")).toInstance(new Address(100, "st"));
 	}
 }
--- src/main/java/com/lisz/PersonModule.java
+++ src/main/java/com/lisz/PersonModule.java
@@ -6,8 +6,7 @@ import com.google.inject.name.Names;
 public class PersonModule extends AbstractModule {
 	@Override
 	protected void configure() {
-		bind(Address.class).annotatedWith(Names.named("address")).toInstance(new Address(100, "st"));
-		//install(new AddressModule());
+		install(new AddressModule());
 		bind(Person.class).toInstance(new Person(1, "shuzheng"));
 	}
 }
