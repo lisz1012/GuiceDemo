071143c7c8ace935a8bec44797b92820d83e853b
lisz1012 <lisz1012@163.com>
2020-07-17 03:36:05 -0700

    Add notes

--- src/main/java/com/lisz/GuiceTest.java
+++ src/main/java/com/lisz/GuiceTest.java
@@ -6,7 +6,7 @@ import com.google.inject.Injector;
 
 import javax.inject.Named;
 
-public class MyTest {
+public class GuiceTest {
 	public static void main(String[] args) {
 		Injector injector = Guice.createInjector(new PersonModule());
 		Person person = injector.getInstance(Person.class);
@@ -14,3 +14,4 @@ public class MyTest {
 	}
 }
 // 打印：Person{id=1, name='shuzheng', address=Address{number=100, street='st'}, company=Company{name='Amazon', companyAddress=Address{number=120, street='Main St.'}}}
+// 测试类（构造方法注入）在：https://github.com/lisz1012/GuiceDemo/tree/master/src/test/java/com/lisz
