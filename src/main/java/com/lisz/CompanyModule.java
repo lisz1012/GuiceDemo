4b133bd6c7a68fb9b18f02c6c9d94f600042c142
lisz1012 <lisz1012@163.com>
2020-07-17 02:35:35 -0700

    Add notes and refactor

--- src/main/java/com/lisz/CompanyModule.java
+++ src/main/java/com/lisz/CompanyModule.java
@@ -6,6 +6,9 @@ import com.google.inject.name.Names;
 public class CompanyModule extends AbstractModule {
 	@Override
 	protected void configure() {
+		// 有@Inject标识的，给Guice注入
 		bind(Address.class).annotatedWith(Names.named("companyAddress")).toInstance(new Address(120, "Main St."));
+		// 没有@Inject标识的，自己通过构造方法传入一个参数
+		bind(Company.class).toInstance(new Company("Amazon"));
 	}
 }
--- src/main/java/com/lisz/PersonModule.java
+++ src/main/java/com/lisz/PersonModule.java
@@ -7,9 +7,16 @@ import com.google.inject.name.Names;
 public class PersonModule extends AbstractModule {
 	@Override
 	protected void configure() {
+		/*
+		 成员变量分成两部分：1. 带有@Inject标识的 2. 没有@Inject标识的
+		 */
+
+		// 有@Inject标识的属性，由Guice注入
 		install(new AddressModule());
 		install(new CompanyModule());
-		bind(Company.class).toInstance(new Company("Amazon"));
+
+		// 成员变量如果不全是被 @Inject 标识的，无@Inject的属性要写一个构造方法，
+		// 然后传参数初始化非@Inject属性，而把@Inject属性留给Guice注入
 		bind(Person.class).toInstance(new Person(1, "shuzheng"));
 	}
 }
